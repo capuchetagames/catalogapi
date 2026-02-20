05b68ae594bbd99a3d0209d2074acaf7ca62c3e7
Diogo Carlomagno <diogocarlomagno@gmail.com>
2026-02-20 11:41:06 -0300

    Centralizes database configuration and setup

--- CatalogApi/Program.cs
+++ CatalogApi/Program.cs
@@ -1,29 +1,19 @@
 using CatalogApi.Config;
-using CatalogApi.Middlewares;
 using CatalogApi.Service;
 using Core.Models;
 using Core.Repository;
 using Infrastructure.Repository;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json").Build();
-
-var connectionString = configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(connectionString);
-}, ServiceLifetime.Scoped);
+builder.Services.AddInfrastructure(builder.Configuration);
 
 // Configuração do cache
 builder.Services.AddMemoryCache();
